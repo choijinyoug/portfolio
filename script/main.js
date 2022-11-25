557e5d4fea4f25e010639ac741c05f1d3b4dd1c2
choijinyoug <112532515+choijinyoug@users.noreply.github.com>
2022-11-25 15:44:38 +0900

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "flip",
+  grabCursor: true,
+  
+  },
+);
+
+
+var swiper = new Swiper(".v_slide", {
+  slidesPerView: 2.5,
+  spaceBetween: 200,
+  centeredSlides: false,
+  pagination: {
+    clickable: true,
+  },
+});
